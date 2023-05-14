2b5774c3ca66a2b700ca75345c1598eb1b1d243e
li000809 <129007049+li000809@users.noreply.github.com>
2023-05-13 23:52:36 -0400

    Delete Web-Form directory

--- Web-Form/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-        <p>&copy; <?php echo date("Y");?>  CST8285.
-        <a href = "https://www.tutorialrepublic.com/php-tutorial/php-mysql-crud-application.php">
-            Updated educational version inspired from tutorialrepublic CRUD sample
-            </a>
-        </p>
-    
-    </footer>
